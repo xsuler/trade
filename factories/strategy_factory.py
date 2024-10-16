b916eb56bad488fa533c053d15be561857b89309
Iyov Su <aeonsule@163.com>
2024-10-16 15:22:10 +0800

    Create strategy_factory.py

---
+++ factories/strategy_factory.py
@@ -0,0 +1,16 @@
+# factories/strategy_factory.py
+
+from typing import Dict
+from strategies.base_strategy import BaseStrategy
+from strategies.ma_crossover_strategy import MovingAverageCrossoverStrategy
+from strategies.rsi_strategy import RSIStrategy
+
+class StrategyFactory:
+    @staticmethod
+    def get_strategy(strategy_name: str, **kwargs) -> BaseStrategy:
+        if strategy_name == 'MovingAverageCrossover':
+            return MovingAverageCrossoverStrategy(**kwargs)
+        elif strategy_name == 'RSI':
+            return RSIStrategy(**kwargs)
+        else:
+            raise ValueError(f"未知的策略名称: {strategy_name}")
